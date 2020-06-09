f39786c8ffb62fe4205d8f3d6af75d54c39c433f
SarahTaylorProject <sarah.taylor@rmit.edu.au>
2020-06-09 11:42:17 +1000

    added basic unique_movie_id sql example

---
+++ example_sql/example_create_unique_movie_id_view.sql
@@ -0,0 +1,9 @@
+﻿DROP VIEW IF EXISTS combined.vw_unique_movie_ids CASCADE;
+CREATE VIEW combined.vw_unique_movie_ids AS
+SELECT movie_id, movie_title, parent_id, max(running_time) as running_time_max
+FROM combined.movie_xml_extract
+GROUP BY movie_id, movie_title, parent_id
+ORDER BY movie_title, movie_id, parent_id;
+
+SELECT * FROM combined.vw_unique_movie_ids;
+
